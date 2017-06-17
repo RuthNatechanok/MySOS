7a4ded1c751c6b2c985fef273dbb3af432d5245d
Admin <ruthnatechanok176@gmail.com>
2017-06-17 14:08:55 +0700

    color ok

---
+++ app/src/main/java/siam/kraicharoen/natechanok/mysos/MainActivity.java
@@ -0,0 +1,13 @@
+package siam.kraicharoen.natechanok.mysos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
