b4b51b14a9e8cb709237df956954fa32992fde64
Admin <ruthnatechanok176@gmail.com>
2017-06-18 10:37:03 +0700

    Intent to NewRegis OK

--- app/src/main/java/siam/kraicharoen/natechanok/mysos/MainActivity.java
+++ app/src/main/java/siam/kraicharoen/natechanok/mysos/MainActivity.java
@@ -1,13 +1,56 @@
 package siam.kraicharoen.natechanok.mysos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit คือการประกาศตัวแปร
+    private EditText userEditText, passwordEditiText;
+    private TextView textView;
+    private Button button;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Initial View
+        initialView();
+
+        //TextView Controller ทำให้textviewสามารถคลิกได้
+        textViewController();
+
+
+
+    }   //Mathod Main นี่คือแมทตอดหลัก
+
+    private void textViewController() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent to NewregisterActivity ลิงค์ไปหน้าสมัครสมาชิก
+                Intent intent = new Intent(MainActivity.this,NewRegisterActivity.class);
+                startActivity(intent);
+
+
+
+            }
+        });
     }
-}
+
+    private void initialView() {
+
+
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditiText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+    }
+}   //Main Class    นี่คือคลาสหลัก
